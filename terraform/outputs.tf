5188bdd744da9bcae1fc3bf158f0d9aa138ea9db
Mut Tulow <mtulow@icloud.com>
2024-03-02 05:17:15 +0000

    Removed non-essential files and folders from module (actually).

--- terraform/outputs.tf
+++
@@ -1,9 +0,0 @@
-# GCP Data Lake
-output "gcp_data_lake" {
-  value = google_storage_bucket.dl.name
-}
-
-# GCP Data Warehouse
-output "gcp_data_warehouse" {
-  value = google_bigquery_dataset.dw.dataset_id
-}
